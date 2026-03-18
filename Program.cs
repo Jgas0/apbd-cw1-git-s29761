2bb21fdf02eb8608a9776131c944b044d445d0b5
Jakub Gasior <s29761@pjwstk.edu.pl>
2026-03-18 18:00:44 +0100

    Dodanie metody

--- Program.cs
+++ Program.cs
@@ -5,4 +5,10 @@ string input = Console.ReadLine();
 if (input == ""){
     Console.WriteLine("Blad");
 }
-public class StatisticsHelper{}
+public class StatisticsHelper
+{
+    public void Metoda()
+    {
+        Console.WriteLine("Dziala");
+    }
+}
