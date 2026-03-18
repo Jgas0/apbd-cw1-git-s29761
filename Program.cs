cf9a8cd1ca373d9857a2f306cc3d5c1c357cd195
Jakub Gasior <s29761@pjwstk.edu.pl>
2026-03-18 19:08:52 +0100

    Metoda CalculateMin

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ public class StatisticsHelper
 {
     public void CalculateAverage(int[] values){}
     public void CalculateMax(int[] values){}
+    public void CalculateMin(int[] values){}
     public void Metoda()
     {
         Console.WriteLine("Dziala");
