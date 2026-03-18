4980ba5194fd095b33c6e9f9305f0abf2b7b0f08
Jakub Gasior <s29761@pjwstk.edu.pl>
2026-03-18 17:59:11 +0100

    Dodanie bledu

--- Program.cs
+++ Program.cs
@@ -2,4 +2,7 @@
 Console.WriteLine("Witaj");
 Console.WriteLine("Podaj liczbe");
 string input = Console.ReadLine();
+if (input == ""){
+    Console.WriteLine("Blad");
+}
 public class StatisticsHelper{}
